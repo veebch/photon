0ab0328ad0dc7943e50c00e5b1259406c69fa382
VEEB <75116070+veebch@users.noreply.github.com>
2022-07-01 13:19:22 +0200

    Add out of range

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ from drivers.ssd1351.ssd1351_16bit import SSD1351 as SSD
 # Values for Fstop, Shutter Speed and ISO
 
 fstops= [.6,.7,.8,.9,1,1.1,1.3,1.4,1.6,1.8,2,2.2,2.5,2.8,3.2,3.6,4,4.5,5,5.6,6.3,7.1,8,9,10,11,13,14,16,18,20,22,25,29,32,36,40,45,51,57,64,72,81,90,102,114,128,144,161]
-sspeed= ["1/ 8000","1/ 6400","1/ 5000","1/ 4000","1/ 3200","1/ 2500","1/ 2000","1/ 1600","1/ 1250","1/ 1000","1/ 800","1/ 640","1/ 500","1/ 400","1/ 320","1/ 250","1/ 200", "1/ 160","1/ 125","1/ 100","1/ 80","1/ 60","1/ 50","1/ 40","1/ 30","1/ 25","1/ 20","1/ 15","1/ 13","1/ 10","1/ 8","1/ 6","1/ 5","1/ 4","0.3","0.4","0.5","0.6","0.8","1","1.3","1.6","2","2.5","3.2","4","5","6","8","10","13","15","20","25","30","40","50","60"]
+sspeed= ["(1/ 8000)","(1/ 6400)","(1/ 5000)","(1/ 4000)","(1/ 3200)","(1/ 2500)","(1/ 2000)","(1/ 1600)","(1/ 1250)","(1/ 1000)","(1/ 800)","(1/ 640)","(1/ 500)","(1/ 400)","(1/ 320)","(1/ 250)","(1/ 200)", "(1/ 160)","(1/ 125)","(1/ 100)","(1/ 80)","(1/ 60)","(1/ 50)","(1/ 40)","(1/ 30)","(1/ 25)","(1/ 20)","(1/ 15)","(1/ 13)","(1/ 10)","(1/ 8)","(1/ 6)","(1/ 5)","(1/ 4)","0.3","0.4","0.5","0.6","0.8","1","1.3","1.6","2","2.5","3.2","4","5","6","8","10","13","15","20","25","30","40","50","60"]
 isonum= [3,4,5,6,8,10,12,16,20,25,32,40,50,64,80,100,125,160,200,250,320,400,500,640,800,1000,1250,1600,2000,2500,3200, 4000, 5000,6400,8000]
 modes= ["AmbientShutterSpeed","AmbientAperture"]
 
@@ -31,8 +31,8 @@ modes= ["AmbientShutterSpeed","AmbientAperture"]
 fstops = list(reversed(fstops))
 
 # For conversiot from ADC to EV
-calib1 = (7900,4.6)            # This is the Calibration that maps a ADC reading to an EV reading from a lightmeter (assumes linearity). 
-calib2 = (8300,6.5)            # EV readings from a Sekonic 558. If components are consistent, these wont need changing.
+calib1 = (9200,7.4)            # This is the Calibration that maps a ADC reading to an EV reading from a lightmeter (assumes linearity). 
+calib2 = (8250,5.7)            # EV readings from a Sekonic 558. If components are consistent, these wont need changing.
 
 #
 #  Invert 2x2 matrix  A = a  b  to solve the simultaneous equation for the straight line
@@ -190,6 +190,7 @@ def adctoreading():
         count = count + 1
     else:
         brightness = sum/n
+    print(brightness)
     EV = brightness * m + i # Assuming linear response in current when exposed to light
     return EV
 
@@ -211,7 +212,8 @@ def displaynum(aperture,speed,iso,mode, isoadjust, lastmeasure):
     except:
         wri.printstring( "err")
     wrimem = CWriter(ssd,freesans20, fgcolor=textT,bgcolor=0, verbose=False)
-    CWriter.set_textpos(ssd, 5,20)  
+    CWriter.set_textpos(ssd, 5,20)
+    speed=str(speed)+" s"
     wrimem.printstring(speed)
     wrimem = CWriter(ssd,freesans20, fgcolor=SSD.rgb(255,255,255),bgcolor=0, verbose=False)
     CWriter.set_textpos(ssd, 35,0)  
@@ -314,19 +316,18 @@ while True:
             isoindex=isoindex + counter
             isoindex=max(0,isoindex)
             isoindex=min(len(isonum)-1,isoindex)
-        else:
-            if mode=="AmbientAperture":
-                apertureindex=apertureindex + counter
-                apertureindex=max(0,apertureindex)
-                apertureindex=min(len(fstops)-1,apertureindex)
-                # Now, derive shutter speed from aperture choice and lastmeasure
-                speedindex = otherindex(apertureindex, isoindex, mode, lastmeasure)
-            elif  mode=='AmbientShutterSpeed':
-                speedindex=speedindex + counter
-                speedindex=max(0,speedindex)
-                speedindex=min(len(sspeed)-1,speedindex)
-                # Now, derive aperture from shutter speed choice and lastmeasure
-                apertureindex = otherindex(speedindex, isoindex, mode, lastmeasure)
+        if mode=="AmbientAperture":
+            apertureindex=apertureindex + counter
+            apertureindex=max(0,apertureindex)
+            apertureindex=min(len(fstops)-1,apertureindex)
+            # Now, derive shutter speed from aperture choice and lastmeasure
+            speedindex = otherindex(apertureindex, isoindex, mode, lastmeasure)
+        elif  mode=='AmbientShutterSpeed':
+            speedindex=speedindex + counter
+            speedindex=max(0,speedindex)
+            speedindex=min(len(sspeed)-1,speedindex)
+            # Now, derive aperture from shutter speed choice and lastmeasure
+            apertureindex = otherindex(speedindex, isoindex, mode, lastmeasure)
         counter=0
     lastcounter=counter
     displaynum(aperture, speed, iso,mode, isoadjust,lastmeasure)
